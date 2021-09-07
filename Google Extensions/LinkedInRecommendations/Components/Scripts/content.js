1fb7bb3a33cafa457e55a7c58403f3bdb3b38b37
sidporwal <siddhartha.porwal@infoedge.com>
2021-09-07 13:04:00 +0530

    Message channel created between content.js and popup.js

--- Google Extensions/LinkedInRecommendations/Components/Scripts/content.js
+++ Google Extensions/LinkedInRecommendations/Components/Scripts/content.js
@@ -1,27 +1,32 @@
 const friendInfo = (e) => {
-    console.log(e);
-  };
-  
-  const ready = () => {
-    let FriendListEl = document.getElementsByClassName(
-      "mn-connection-card__name"
-    );
-    let FriendActionsEl = document.getElementsByClassName(
-      "mn-connection-card__action-container"
-    );
-    console.log(FriendListEl);
-    console.log(FriendListEl.length);
-    for (let i = 0; i < FriendListEl.length; i++) {
-      console.log(FriendListEl[i].innerText);
-      let newButton = document.createElement("button");
-      newButton.setAttribute("id", FriendListEl[i].innerText);
-      newButton.className = "recomend";
-      newButton.innerText = "Recommend";
-      newButton.addEventListener("click", friendInfo);
-      FriendActionsEl[i].appendChild(newButton);
+  console.log(e);
+};
+
+const ready = () => {
+  let FriendListEl = document.getElementsByClassName(
+    "mn-connection-card__name"
+  );
+  let FriendActionsEl = document.getElementsByClassName(
+    "mn-connection-card__action-container"
+  );
+  console.log(FriendListEl);
+  console.log(FriendListEl.length);
+  for (let i = 0; i < FriendListEl.length; i++) {
+    console.log(FriendListEl[i].innerText);
+    let newButton = document.createElement("button");
+    newButton.setAttribute("id", FriendListEl[i].innerText);
+    newButton.className = "recomend";
+    newButton.innerText = "Recommend";
+    newButton.addEventListener("click", friendInfo);
+    FriendActionsEl[i].appendChild(newButton);
+  }
+  // send message back to popup script
+  chrome.runtime.sendMessage(
+    { data: "Hello popup, how are you" },
+    (response) => {
+      console.log(response);
     }
-    // send message back to popup script
-    chrome.runtime.sendMessage(null, "Yes");
-  };
-  
-  setTimeout(ready, 3000); 
+  );
+};
+
+setTimeout(ready, 3000);
