315856adebef5028faa3f2c7bf3d2727409c649d
kumar.vivek <kumar.vivek@naukri.com>
2021-09-12 23:00:05 +0530

    Vivek 'Open Side Modal'

--- Google Extensions/LinkedInRecommendations/Components/Scripts/popup.js
+++ Google Extensions/LinkedInRecommendations/Components/Scripts/popup.js
@@ -1,61 +1,83 @@
 startNavigation.onclick = function (element) {
   // query the current tab to find its id
-  chrome.tabs.query(
-    { active: true, currentWindow: true },
-    function (tabs) {
-      const url = "https://www.linkedin.com/mynetwork/invite-connect/connections/";
-      const tab_id = tabs[0].id;
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const url =
+      "https://www.linkedin.com/mynetwork/invite-connect/connections/";
+    const tab_id = tabs[0].id;
 
-      // update current tab with new url
-      chrome.tabs.update({ url: url });
+    // update current tab with new url
+    chrome.tabs.update({ url: url });
 
-      // fired when tab is updated
-      chrome.tabs.onUpdated.addListener(function openPage(tabID, changeInfo) {
-        // tab has finished loading, validate whether it is the same tab
-        if (tab_id == tabID && changeInfo.status === "complete") {
-          // remove tab onUpdate event as it may get duplicated
-          chrome.tabs.onUpdated.removeListener(openPage);
+    // fired when tab is updated
+    chrome.tabs.onUpdated.addListener(function openPage(tabID, changeInfo) {
+      // tab has finished loading, validate whether it is the same tab
+      if (tab_id == tabID && changeInfo.status === "complete") {
+        // remove tab onUpdate event as it may get duplicated
+        chrome.tabs.onUpdated.removeListener(openPage);
 
-          // execute content script
-          chrome.scripting.executeScript(
-            {
-              target: { tabId: tabID, allFrames: true },
-              files: ["./Components/Scripts/fetchConnections.js"],
-            },
-          );
-        }
-      });
-    }
-  );
+        // execute content script
+        chrome.scripting.executeScript({
+          target: { tabId: tabID, allFrames: true },
+          files: ["./Components/Scripts/fetchConnections.js"],
+        });
+      }
+    });
+  });
 };
 
 startFetchingUserInfo.onclick = function (element) {
   // query the current tab to find its id
-  chrome.tabs.query(
-    { active: true, currentWindow: true },
-    function (tabs) {
-      const url = "https://www.linkedin.com/mynetwork/invite-connect/connections/";
-      const tab_id = tabs[0].id;
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const url =
+      "https://www.linkedin.com/mynetwork/invite-connect/connections/";
+    const tab_id = tabs[0].id;
 
-      // update current tab with new url
-      chrome.tabs.update({ url: url });
+    // update current tab with new url
+    chrome.tabs.update({ url: url });
 
-      // fired when tab is updated
-      chrome.tabs.onUpdated.addListener(function openPage(tabID, changeInfo) {
-        // tab has finished loading, validate whether it is the same tab
-        if (tab_id == tabID && changeInfo.status === "complete") {
-          // remove tab onUpdate event as it may get duplicated
-          chrome.tabs.onUpdated.removeListener(openPage);
+    // fired when tab is updated
+    chrome.tabs.onUpdated.addListener(function openPage(tabID, changeInfo) {
+      // tab has finished loading, validate whether it is the same tab
+      if (tab_id == tabID && changeInfo.status === "complete") {
+        // remove tab onUpdate event as it may get duplicated
+        chrome.tabs.onUpdated.removeListener(openPage);
 
-          // execute content script
-          chrome.scripting.executeScript(
-            {
-              target: { tabId: tabID, allFrames: true },
-              files: ["./Components/Scripts/fetchUserInfo.js"],
-            },
-          );
-        }
-      });
-    }
-  );
-}
+        // execute content script
+        chrome.scripting.executeScript({
+          target: { tabId: tabID, allFrames: true },
+          files: [
+            "./Components/Scripts/fetchUserInfo.js",
+            "./Components/Scripts/injectSideBar.js",
+          ],
+        });
+      }
+    });
+  });
+};
+
+OpenSideBar.onclick = function (element) {
+  // query the current tab to find its id
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const url =
+      "https://www.linkedin.com/mynetwork/invite-connect/connections/";
+    const tab_id = tabs[0].id;
+
+    // update current tab with new url
+    chrome.tabs.update({ url: url });
+
+    // fired when tab is updated
+    chrome.tabs.onUpdated.addListener(function openPage(tabID, changeInfo) {
+      // tab has finished loading, validate whether it is the same tab
+      if (tab_id == tabID && changeInfo.status === "complete") {
+        // remove tab onUpdate event as it may get duplicated
+        chrome.tabs.onUpdated.removeListener(openPage);
+
+        // execute content script
+        chrome.scripting.executeScript({
+          target: { tabId: tabID, allFrames: true },
+          files: ["./Components/Scripts/injectSideBar.js"],
+        });
+      }
+    });
+  });
+};
