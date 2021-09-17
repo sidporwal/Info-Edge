01662939f6f850c32060a5954f781743a550fe76
sidporwal <siddhartha.porwal@infoedge.com>
2021-09-17 09:21:31 +0530

    Implementing dummy api in extensions

--- Google Extensions/LinkedInRecommendations/Components/Scripts/injectSideBar.js
+++ Google Extensions/LinkedInRecommendations/Components/Scripts/injectSideBar.js
@@ -1,6 +1,17 @@
 const HTML_TEMPLATE =
   '<html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width"><title>Popup</title><style>*{margin:0;padding:0;border:0;outline:0}.RSPBstep{display:inline-flex;justify-content:center;align-items:center;z-index:0;position:absolute;transform:translateX(-50%);transition-property:all;transition-timing-function:ease}.RSPBprogression{position:absolute;transition:width .3s ease;left:0;top:0;bottom:0;border-radius:10px;background:#ffa800;z-index:-1;height:10px}.step-progress-titles-section{color:#fff;display:flex;font-weight:700;margin-top:16px}.step-progress-title{flex:1;align-content:center;text-align:center;font-size:10px}.main-outer{height:100%;text-align:center}.screen-decor{margin-left:16px;margin-right:16px;height:100%;margin-top:8%;color:#fff}.onboarding-button{background-color:#e29706;width:90%;height:48px;margin-top:64px;color:#fff;border-radius:8px;font-weight:500;font-size:medium;padding:5px 10px;cursor:pointer;-webkit-transition-duration:.4s;transition-duration:.4s}.onboarding-button:hover{transform:scale(1.1,1.3)}.small-heading-text{font-size:small}.indexedStep{color:#fff;width:32px;height:32px;font-size:16px;background-color:#0e88ff;border-radius:50%;display:flex;justify-content:center;align-items:center}.accomplished{background-color:#0e88ff}.step-progress-outer{margin-left:32px;margin-right:32px;margin-top:8px}.RSPBprogressBar{height:10px;line-height:1;border-radius:10px;position:relative;background-color:#0e88ff;display:flex;justify-content:space-between;align-items:center;z-index:0}</style></head><body style="background-color:#0f1216"><div id="popup-root"><section id="popup" class="main-section" style="height:949px"><section><div class="step-progress-outer"><div class="RSPBprogressBar" style="background:#0e88ff;height:10px;margin-top:20px"><div class="RSPBstep" style="left:0;transition-duration:300ms;transform:translateX(-50%) scale(1)"><div class="indexedStep accomplished">1</div></div><div class="RSPBstep" style="left:25%;transition-duration:300ms;transform:translateX(-50%) scale(1)"><div class="indexedStep accomplished">2</div></div><div class="RSPBstep" style="left:50%;transition-duration:300ms;transform:translateX(-50%) scale(1)"><div class="indexedStep accomplished">3</div></div><div class="RSPBstep" style="left:75%;transition-duration:300ms;transform:translateX(-50%) scale(1)"><div class="indexedStep accomplished">4</div></div><div class="RSPBstep" style="left:100%;transition-duration:300ms;transform:translateX(-50%) scale(1)"><div class="indexedStep null">5</div></div><div class="RSPBprogression" style="background:linear-gradient(toright,#55efc4,#55efc4);width:0"></div></div></div><div><section class="step-progress-titles-section"><h3 class="step-progress-title">Install<br>Extension</h3><h3 class="step-progress-title">Go to<br>Linkedin</h3><h3 class="step-progress-title">Sync<br>contacts</h3><h3 class="step-progress-title">Vouch for<br>friends</h3><h3 class="step-progress-title">Track<br></h3></section></div><div class="main-outer"><section class="screen-decor" style="background-position:70%40%"><h3 style="margin-top:20%;font-size:22px;margin-left:16px;font-weight:350"><span>Vouch for engineers from LinkedIn and earn</span></h3><h3 style="font-size:16px;margin-top:32px;margin-left:16px"><span style="color:#ff565e">👉 ₹10000</span> for successful hire<br></h3><h3 style="font-size:16px;margin-top:32px;margin-left:16px"><span style="color:#ff565e">👉 ₹1000</span> per interviewed candidate<br></h3><h3 style="font-size:16px;margin-top:32px;margin-left:16px"><span style="color:#ff565e">👉 ₹100</span> for vouching connections<br></h3><button id="getListBtn" class="onboarding-button" style="margin-top:64px;opacity:.5;cursor:not-allowed">Fetching Your LinkedIn Data</button><h1 class="small-heading-text" style="margin:16px;text-align:center">We are 100% secure.</h1></section></div></section></section></div></body></html>';
 
+const sendDataUsingAPIs = ({ from, subject, data }) => {
+  chrome.runtime.sendMessage({ from, subject, data });
+};
+
+const showProgress = ({ base, completed }) => {
+  const documentX = document.getElementById("dibsFrame").contentDocument;
+  documentX.getElementsByClassName("RSPBprogression")[0].style = `width: ${
+    base + completed / 4
+  }%;`;
+};
+
 const fetchUserContactInfo = (iframeId) => {
   return new Promise((resolve, reject) => {
     try {
@@ -57,9 +68,14 @@ const goToProfilePage = (iframeId) => {
               "ember-view link-without-visited-state cursor-pointer text-heading-small inline-block break-words"
             )[0]
             .click();
-          fetchUserContactInfo(iframeId).then((data) => {
-            resolve(data);
-          });
+          fetchUserContactInfo(iframeId)
+            .then((data) => {
+              resolve(data);
+            })
+            .catch((error) => {
+              console.error();
+              reject(error);
+            });
         }
       }, 1000);
     } catch (error) {
@@ -94,9 +110,14 @@ const fetchUserInfo = () => {
                   "ember-view link-without-hover-state"
                 )[0]
                 .click();
-              goToProfilePage("userInfoFrame").then((data) => {
-                resolve(data);
-              });
+              goToProfilePage("userInfoFrame")
+                .then((data) => {
+                  resolve(data);
+                })
+                .catch((error) => {
+                  console.error(error);
+                  reject(error);
+                });
               clearInterval(intervalId2);
             }
           }, 1000);
@@ -120,6 +141,7 @@ const getUserInfoFrame = async () => {
   try {
     const data = await fetchUserInfo();
     // API Post Current Use
+    sendDataUsingAPIs({ from: "content", subject: "userInfo", data });
   } catch (error) {
     console.error(error);
   } finally {
@@ -138,13 +160,17 @@ const fetchConnectionMail = (profileUrl) => {
       "position: fixed; top: 200px; right: 0px; height: 500px; width: 500px; z-index: -9999;";
     document.body.appendChild(iframe);
     try {
-      fetchUserContactInfo(profileUrl).then((values) => {
-        const data = values;
-        setTimeout(() => {
-          document.body.removeChild(iframe);
-          resolve(data);
-        }, 5000);
-      });
+      fetchUserContactInfo(profileUrl)
+        .then((values) => {
+          const data = values;
+          setTimeout(() => {
+            document.body.removeChild(iframe);
+            resolve(data);
+          }, 5000);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     } catch (error) {
       console.error(error);
     }
@@ -153,21 +179,36 @@ const fetchConnectionMail = (profileUrl) => {
 
 let connectionsMail = [];
 const fetchConnectionMails = async (data, i) => {
-  if (i === data.length) {
+  showProgress({ base: 50, completed: (i / data.length) * 100 });
+  try {
+    if (i === data.length) {
+      console.log(connectionsMail);
+      sendDataUsingAPIs({
+        from: "content",
+        subject: "connectionsMailInfo",
+        data: connectionsMail,
+      });
+      return;
+    }
+    if (i % 10 === 0 && i != 0) {
+      console.log(connectionsMail);
+      sendDataUsingAPIs({
+        from: "content",
+        subject: "connectionsMailInfo",
+        data: connectionsMail,
+      });
+      connectionsMail = [];
+    }
+    const contactData = await fetchConnectionMail(data[i].profileUrl);
+    connectionsMail.push(contactData);
     console.log(connectionsMail);
-    return;
+    fetchConnectionMails(data, i + 1);
+  } catch (error) {
+    console.error(error);
   }
-  if (i % 10 === 0 && i != 0) {
-    console.log(connectionsMail);
-    connectionsMail = [];
-  }
-  const contactData = await fetchConnectionMail(data[i].profileUrl);
-  connectionsMail.push(contactData);
-  console.log(connectionsMail);
-  fetchConnectionMails(data, i + 1);
 };
 
-const sendConnectionData = (prev, curr) => {
+const sendConnectionData = (prev, curr, isApiCall = true) => {
   const documentX = document.getElementById(
     "userConnectionFrame"
   ).contentDocument;
@@ -194,6 +235,9 @@ const sendConnectionData = (prev, curr) => {
     data.push(userInfo);
   }
   console.log(data);
+  if (isApiCall) {
+    sendDataUsingAPIs({ from: "content", subject: "connectionsInfo", data });
+  }
   return data;
 };
 
@@ -226,6 +270,7 @@ const fetchFriends = (connLen) => {
       } else {
         i = 0;
         sendConnectionData(prevLen, currLen);
+        showProgress({ base: 25, completed: (currLen / connLen) * 100 });
       }
       console.log(FriendListEl.length);
       if (FriendListEl.length == parseInt(connLen) || i === 60) {
@@ -253,11 +298,12 @@ const ready = () => {
             const FriendListElName = documentX.getElementsByClassName(
               "mn-connection-card__name"
             );
-            const data = sendConnectionData(0, FriendListElName.length);
+            const data = sendConnectionData(0, FriendListElName.length, false);
             const documentXY =
               document.getElementById("dibsFrame").contentDocument;
             documentXY.getElementsByClassName("RSPBprogression")[0].style =
               "width: 75%";
+            getBtnState(true);
             fetchConnectionMails(data, 0);
             resolve();
           });
@@ -330,7 +376,6 @@ const startServicesId = setInterval(() => {
   if (document) {
     getSideBanner();
     getUserInfoFrame().then(() => {
-      getBtnState(true);
       userConnectionFrame();
     });
     clearInterval(startServicesId);
--- Google Extensions/LinkedInRecommendations/service-worker.js
+++ Google Extensions/LinkedInRecommendations/service-worker.js
@@ -10,12 +10,32 @@ chrome.action.onClicked.addListener(() => {
   });
 });
 
-// chrome.runtime.onMessage.addListener((msg, sender) => {
-//   // First, validate the message's structure.
-//   if ((msg.from === 'content') && (msg.subject === 'showPageAction')) {
-//     // Enable the page-action for the requesting tab.
-//     chrome.pageAction.show(sender.tab.id);
-//   }
-// });
+let currentUserInfo;
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  // First, validate the message's structure.
+  if (msg.from === "content" && msg.subject === "userInfo") {
+    currentUserInfo = msg.data;
+    console.log(msg.data);
+    fetch("http://10.120.9.102:8081/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        token: currentUserInfo.mail,
+      },
+      body: JSON.stringify(currentUserInfo),
+    })
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  }
+  if (msg.from === "content" && msg.subject === "connectionsInfo") {
+    console.log(currentUserInfo.mail);
+    console.log(msg.data);
+  }
+  if (msg.from === "content" && msg.subject === "connectionsMailInfo") {
+    console.log(currentUserInfo.mail);
+    console.log(msg.data);
+  }
+});
 
 // chrome.extension.getURL('popup.html')
