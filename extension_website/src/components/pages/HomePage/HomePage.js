bb166e90e933d9b991ce4ae0fe1b94b5bad866df
kumar.vivek <kumar.vivek@naukri.com>
2021-09-18 01:22:25 +0530

    Zombie 'HomePage Created'

--- extension_website/src/components/pages/HomePage/HomePage.js
+++ extension_website/src/components/pages/HomePage/HomePage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import routeConfig from "../../../constants/routeConfig";
+import Navigation from "../../molecules/Navigation/Navigation";
+
+import "./HomePage.css";
 
 const HomePage = (props) => {
   const { location, history } = props;
@@ -10,7 +13,39 @@ const HomePage = (props) => {
     localStorage.setItem("userMail", mail);
     history.push(routeConfig.selection);
   }
-  return <div>HomePage</div>;
+  return (
+    <div className="homepage">
+      <Navigation />
+      <img
+        src="https://uploads-ssl.webflow.com/602ceee1cc4788ef7fdd0ea4/602d15fbdbbdf2b154a1b10a_shape-3.png"
+        loading="lazy"
+        alt=""
+        class="shape-1"
+      />
+      <img
+        src="https://uploads-ssl.webflow.com/602ceee1cc4788ef7fdd0ea4/602d15fbdbbdf2b154a1b10a_shape-3.png"
+        loading="lazy"
+        alt=""
+        class="shape-2"
+      />
+      <img
+        src="https://uploads-ssl.webflow.com/602ceee1cc4788ef7fdd0ea4/602d15fbdbbdf2b154a1b10a_shape-3.png"
+        loading="lazy"
+        alt=""
+        class="shape-3"
+      />
+      <div className="centerText">
+        <h4 class="sub-small-title">Help friends find jobs, make money</h4>
+        <h1 className="homepageHeading">
+          Earn assured rewards passive income by recommending friends
+        </h1>
+        <p class="subhead">
+          Be a scout and get your software engineer friends hired by top
+          companies
+        </p>
+      </div>
+    </div>
+  );
 };
 
 HomePage.propTypes = {
