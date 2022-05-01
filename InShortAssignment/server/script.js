b4006221e4a31300f698fb1f41668d45bd3966d1
sidporwal <siddhartha.porwal@infoedge.com>
2022-05-02 00:39:19 +0530

    Add production build in server directory

--- InShortAssignment/server/script.js
+++ InShortAssignment/server/script.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const scrapCovidData = require("./utils/scrapCovidData");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use((req, res, next) => {
   res.set("Access-Control-Allow-Origin", "*");
@@ -17,6 +20,10 @@ app.get("/api/covidData", async (req, res) => {
   } catch (error) {
     res.status(404).send({ message: "No data available" });
   }
+});
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const port = process.env.port || 8080;
